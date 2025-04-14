14729c0a693974aa90670a80839bd21b8833de12
Leonardo Gama <leonardogama2000@outlook.com.br>
2025-04-14 06:21:45 -0400

    Alterando program.cs

--- popfragg.Api/Program.cs
+++ popfragg.Api/Program.cs
@@ -19,51 +19,12 @@ builder.Host.UseSerilog();
 builder.Services.ConfigureServices();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-
-//TODO COLOCAR EM UM ARQUIVO SEPARADO CLIENTES HTTPS PERSONALIZADOS
-builder.Services.AddHttpClient(HttpClientNames.Steam, client =>
-{
-    client.BaseAddress = new Uri("https://steamcommunity.com/openid/");
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
-
-builder.Services.AddHttpClient(HttpClientNames.Authorizer, client =>
-{
-    client.BaseAddress = new Uri("https://authorizer-production-a43d.up.railway.app");
-    client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
-
+builder.Services.ConfigureHttpClients();
 var connectionStrings = new EnvironmentConfig(builder.Configuration, builder.Environment.EnvironmentName);
 builder.Services.AddAuthorizerService(connectionStrings);
-
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "From Shot",
-        Version = "v1",
-        Description = "API Cameponatos e-sports",
-        Contact = new OpenApiContact
-        {
-            Name = "Leonardo Gama",
-            Email = "leonardogama2000@outlook.com.br",
-            Url = new Uri("https://github.com")
-        }
-    });
-});
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:4200","https://popfragg.vercel.app") // Substitua pela sua origem real
-                   .AllowAnyHeader()
-                   .AllowAnyMethod()
-                    .AllowCredentials(); // Permite o envio de cookies
-        });
-});
+builder.Services.ConfigureMiddleware();
+builder.Services.ConfigureSwagger();
+builder.Services.ConfigureCors();
 
 // Força Kestrel a escutar na porta 80 (padrão do container)
 builder.WebHost.ConfigureKestrel(serverOptions =>
@@ -77,24 +38,20 @@ var app = builder.Build();
 // Configuração do pipeline de requisições
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FromShot v1");
-        c.RoutePrefix = "swagger";
-    });
+    app.AddSwagger();
     
 }
 
-
 // Adiciona HTTPS
-//app.UseHttpsRedirection();
+if(app.Environment.IsDevelopment() || app.Environment.IsStaging())
+{
+    app.UseHttpsRedirection();
+}
 
 // Adiciona CORS
 app.UseCors("AllowSpecificOrigin");
 
 // Middleware de tratamento de erros
-//app.UseExceptionHandler("/error");
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseStatusCodePages();
 
